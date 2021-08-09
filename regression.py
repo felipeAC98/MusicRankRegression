4095b545acb0d1ddfbed4b76e7876160c56c78ac
FelipeAC98 <felipe.alvcor@gmail.com>
2021-08-09 00:32:49 -0300

    Implementacao regressao KNN

--- regression.py
+++ regression.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn import neighbors
-
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn import metrics
 
 #Obtendo dados
 matchSpotify4Mula =("matchSpotify4Mula.csv")
@@ -10,3 +12,20 @@ features=['music_id', 'music_name', 'music_lang', 'art_id','art_name', 'art_rank
 X = pd.read_csv(matchSpotify4Mula, sep=',', names = features)
 
 print(X.head())
+
+# Fit regression model
+n_neighbors = 5
+
+knn = neighbors.KNeighborsRegressor(n_neighbors)
+Y = X['mus_rank']
+X=X.drop(columns=['mus_rank'])
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+
+knnFit = knn.fit(x_train, y_train)
+
+preResult=knnFit.predict(x_test)
+
+acuracia = metrics.accuracy_score(y_test, preResult)
+
+print(acuracia)
