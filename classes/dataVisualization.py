033b486d06f9c6cad7cec4aeaed8cafdf425a7ad
FelipeAC98 <felipe.alvcor@gmail.com>
2021-12-03 00:13:24 -0300

    Criacao de mais alguns plots uteis para analise

--- classes/dataVisualization.py
+++ classes/dataVisualization.py
@@ -21,19 +21,27 @@ class data_visualization():
 	def save_figure(self,figID,figureName):
 		self.plt.savefig(str(self.path)+str(figID)+'-'+figureName)
 
-	def plot_distribution(self,targetFeature="popularity",**histplot):
-		self.plt.title("Distribuicao das musicas por posicao")
+	def plot_distribution(self,targetFeature="popularity",length=50, height=30):
+		self.plt.title("Distribuicao das musicas por popularidade")
 		self.plt.xlabel(targetFeature)
 		self.plt.ylabel("Quantidade de musicas")
 
 		sns.histplot(x = targetFeature, data =self.musicData.df, kde=True)
 		self.save_figure(targetFeature,"plot_distribution")
 
-	def plot_qtd_musics_by(self,targetFeature):
+	def plot_qtd_musics_by(self,targetFeature,length=50, height=30):
 		self.plt.title("Quantidade de musicas por '"+str(targetFeature)+ "'")
 		self.plt.xlabel(targetFeature)
 		self.plt.ylabel("Quantidade de musicas")
-		self.plt.figure(figsize=(50, 30))
+		self.set_figure_size(length=length, height=height)
 
 		sns.countplot(x = targetFeature, data =self.musicData.df)
 		self.save_figure(targetFeature,"plot_qtd_musics_by")
+
+	def plot_boxplot(self,targetFeature1,targetFeature2,length=20, height=30):
+
+		#f, axes = plt.subplots(2, 2, figsize=(15, 15), sharex=False)
+		#sns.despine(left=True)
+		self.set_figure_size(length=length, height=height)
+		sns.boxplot(targetFeature1, targetFeature2, data = self.musicData.df)
+		self.save_figure(str(targetFeature1)+'_by_'+str(targetFeature2),"plot_boxplot")
--- plots_generation.py
+++ plots_generation.py
@@ -9,10 +9,6 @@ import copy
 
 musicData=get_prep_mus_data()
 
-#Aplicando one hot enconding
-#musicData.df = transform.useOneHotEncoder(musicData.df, 'main_genre','genre-')
-#musicData.df = transform.useOneHotEncoder(musicData.df, 'music_lang')
-
 #obtendo release time - tempo em meses em que a musica foi lancada
 try:
 	musicData.df = transform.monthsAfterRelease(musicData.df,'release_date')
@@ -22,6 +18,13 @@ except:
 
 dataVisualization=data_visualization(musicData)
 
-dataVisualization.plot_distribution(targetFeature="popularity")
+#dataVisualization.plot_distribution(targetFeature="popularity")
 
-dataVisualization.plot_qtd_musics_by(targetFeature="main_genre")
+dataVisualization.plot_qtd_musics_by(targetFeature="main_genre",length=60, height=20)
+
+#dataVisualization.plot_qtd_musics_by(targetFeature="release_date")
+
+#dataVisualization.plot_boxplot(targetFeature1="popularity",targetFeature2="main_genre",length=20, height=30)
+
+#Precisa corrigir, mas eh um plot interessante
+#dataVisualization.plot_boxplot(targetFeature1="release_date",targetFeature2="popularity")
