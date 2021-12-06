e5f548ae2d3abe2fe7bf398af4093691fc433360
FelipeAC98 <felipe.alvcor@gmail.com>
2021-12-05 22:43:40 -0300

    Criacao codigo e classe shap_values

---
+++ classes/analytics.py
@@ -0,0 +1,15 @@
+import shap
+import matplotlib.pyplot as plt
+
+class shap_values():
+
+	def __init__(self,regressor):
+		self.regressor=regressor
+
+	def tree_explainer(self):
+		shap_values = shap.TreeExplainer(self.regressor.model).shap_values(self.regressor.musicData.xTrain)
+		shap.summary_plot(shap_values, self.regressor.musicData.xTrain, plot_type="bar")		
+
+		f = plt.figure()
+		shap.summary_plot(shap_values , self.regressor.musicData.xTrain, plot_type="bar")
+		f.savefig("summary_plot1.png", bbox_inches='tight', dpi=600)
---
+++ shap_values.py
@@ -0,0 +1,33 @@
+from classes.musicData import music_data_entity, get_prep_mus_data
+import classes.regressor
+from classes.analytics import shap_values
+import transform
+import numpy as np
+
+musicData=get_prep_mus_data()
+
+#Aplicando one hot enconding
+musicData.df = transform.useOneHotEncoder(musicData.df, 'main_genre','genre-')
+musicData.df = transform.useOneHotEncoder(musicData.df, 'music_lang')
+
+#obtendo release time - tempo em meses em que a musica foi lancada
+try:
+	musicData.df = transform.monthsAfterRelease(musicData.df,'release_date')
+
+except:
+	print(' release_time nao encontrada: '+str(traceback.format_exc()))
+
+#removendo algumas amostras para deixar o treinamento inicial mais rapido
+nIndexsToDrop=int(len(musicData.df.index)*0.7)
+
+indexsToDrop = np.random.choice(musicData.df.index, nIndexsToDrop, replace=False)
+musicData.df = musicData.df.drop(indexsToDrop)	
+
+musicData.train_test_split(targetFeatureName="popularity")
+
+#Extra forest regressor
+randon_extra_tree_regressor=classes.regressor.randon_extra_tree_regressor(musicData,min_impurity_decrease=0.0005)
+
+shapValues=shap_values(randon_extra_tree_regressor)
+
+shapValues.tree_explainer()
