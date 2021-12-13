c61a49259d369d1cd426690e5085b8e861593621
FelipeAC98 <felipe.alvcor@gmail.com>
2021-12-12 23:36:29 -0300

    Criacao funcao para arvore de decisao e plot
    Plot efetuado utilizando funcao do graphviz, basta chamar a funcao
    plot_tree

--- classes/regressor.py
+++ classes/regressor.py
@@ -1,6 +1,9 @@
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import ExtraTreesRegressor 
+from sklearn import tree
+import graphviz 
+import pylab
 
 class regressor():
 
@@ -23,6 +26,24 @@ class knn_regressor(regressor):
 		self.model=KNeighborsRegressor(**params) #**despactando o dict para mandar os parametros para a funcao interna
 		self.fit()
 		self.params=params
+
+class tree_regressor(regressor):
+
+	def __init__(self, musicData, **params):
+		super().__init__(musicData)
+		self.model=tree.DecisionTreeRegressor(**params)
+		self.fit()
+		self.params=params
+
+	def plot_tree(self):
+		dot_data = tree.export_graphviz(self.model, out_file=None, 
+                    feature_names= self.musicData.df.columns,  
+                    filled=True, rounded=True,  
+                    special_characters=True)  
+
+		graph = graphviz.Source(dot_data) 
+		graph.render("decision_tree") 
+		pylab.savefig('decisionTree.png')
 
 class randon_forest_regressor(regressor):
 
