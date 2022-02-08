a5fc170f82e82e969df3329f203d281ead39ff4d
FelipeAC98 <felipe.alvcor@gmail.com>
2022-02-08 19:13:32 -0300

    Adicao funcao de plot de decisao shapvalues

--- classes/analytics.py
+++ classes/analytics.py
@@ -24,3 +24,14 @@ class shap_values():
 		f = plt.figure()
 		shap.summary_plot(shap_values , self.regressor.musicData.xTrain)
 		f.savefig("newPlots/"+str(name)+"-explainer_default.png", bbox_inches='tight', dpi=600)
+
+	def decision_plot(self,name,nSamples=100):
+		explainer = shap.TreeExplainer(self.regressor.get_model())
+		shapValues =explainer.shap_values(self.regressor.musicData.xTest)
+
+		f = plt.figure()
+		#features = musicData.df.iloc[select]
+		#featuresDisplay = X_display.loc[nSamples]
+		shap.decision_plot(explainer.expected_value[:nSamples], shapValues[:nSamples], self.regressor.musicData.xTest,ignore_warnings=True)
+		#shap.summary_plot(shapValues , self.regressor.musicData.xTrain)
+		f.savefig("newPlots/"+str(name)+"-decision_plot.png", bbox_inches='tight', dpi=600)
