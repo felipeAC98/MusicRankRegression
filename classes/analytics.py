0dc0bfe768b035a25aec6adf070d2589de89d82c
FelipeAC98 <felipe.alvcor@gmail.com>
2022-02-07 23:10:02 -0300

    Correcao de plot shap values

--- classes/analytics.py
+++ classes/analytics.py
@@ -6,22 +6,21 @@ class shap_values():
 	def __init__(self,regressor):
 		self.regressor=regressor
 
-	def tree_explainer(self):
+	def tree_explainer(self,name):
 		shap_values = shap.TreeExplainer(self.regressor.model).shap_values(self.regressor.musicData.xTrain)
 		#shap.summary_plot(shap_values, self.regressor.musicData.xTrain, plot_type="bar")		
-
 		f = plt.figure()
 		shap.summary_plot(shap_values , self.regressor.musicData.xTrain, plot_type="bar")
-		f.savefig("summary_plot1.png", bbox_inches='tight', dpi=600)
+		f.savefig("newPlots/"+str(name)+"-tree_explainer.png", bbox_inches='tight', dpi=600)
 
-	def explainer(self):
-		shap_values = shap.Explainer(self.regressor.model).shap_values(self.regressor.musicData.xTrain)
+	def explainer(self,name):
+		shap_values = shap.Explainer(self.regressor.get_model()).shap_values(self.regressor.musicData.xTrain)
 		f = plt.figure()
 		shap.summary_plot(shap_values , self.regressor.musicData.xTrain, plot_type="bar")
-		f.savefig("summary_plot1.png", bbox_inches='tight', dpi=600)
+		f.savefig("newPlots/"+str(name)+"-explainer.png", bbox_inches='tight', dpi=600)
 
-	def explainer_default(self):
-		shap_values = shap.Explainer(self.regressor.model).shap_values(self.regressor.musicData.xTrain)
+	def explainer_default(self,name):
+		shap_values = shap.Explainer(self.regressor.get_model()).shap_values(self.regressor.musicData.xTrain)
 		f = plt.figure()
 		shap.summary_plot(shap_values , self.regressor.musicData.xTrain)
-		f.savefig("summary_plot1.png", bbox_inches='tight', dpi=600)
+		f.savefig("newPlots/"+str(name)+"-explainer_default.png", bbox_inches='tight', dpi=600)
--- shap_values.py
+++ shap_values.py
@@ -6,11 +6,13 @@ import numpy as np
 
 musicData=get_prep_mus_data()
 
+print(musicData.df.head())
 #Aplicando one hot enconding
 musicData.df = transform.useOneHotEncoder(musicData.df, 'main_genre','genre-')
 musicData.df = transform.useOneHotEncoder(musicData.df, 'music_lang')
 #musicData.drop_columns(['music_lang-pt-br'])
 #musicData.drop_columns(['totalFollowers'])
+print(musicData.df.head())
 
 #obtendo release time - tempo em meses em que a musica foi lancada
 try:
@@ -20,19 +22,25 @@ except:
 	print(' release_time nao encontrada: '+str(traceback.format_exc()))
 
 #removendo algumas amostras para deixar o treinamento inicial mais rapido
-nIndexsToDrop=int(len(musicData.df.index)*0.9)
+#nIndexsToDrop=int(len(musicData.df.index)*0.9)
 
-indexsToDrop = np.random.choice(musicData.df.index, nIndexsToDrop, replace=False)
-musicData.df = musicData.df.drop(indexsToDrop)	
+#indexsToDrop = np.random.choice(musicData.df.index, nIndexsToDrop, replace=False)
+#musicData.df = musicData.df.drop(indexsToDrop)	
 
 musicData.train_test_split(targetFeatureName="popularity")
 
-#Extra forest regressor
-randon_extra_tree_regressor=classes.regressor.randon_extra_tree_regressor(musicData,min_impurity_decrease=0.0005)
-print("Extra score: "+str(randon_extra_tree_regressor.get_score()))	
+#Tree score
+tree_regressor=classes.regressor.tree_regressor(musicData,min_impurity_decrease=0.2)
+tree_regressor.fit()
+shapValues=shap_values(tree_regressor)
+shapValues.tree_explainer("tree_regressor")
+shapValues.explainer("tree_regressor")
+shapValues.explainer_default("tree_regressor")
 
-shapValues=shap_values(randon_extra_tree_regressor)
-
+#Random forest score
+#randon_forest_regressor=classes.regressor.randon_forest_regressor(musicData,min_impurity_decrease=0.001,n_estimators=400)
+#randon_forest_regressor.fit()
+#shapValues=shap_values(knn_regressor)
 #shapValues.tree_explainer()
 #shapValues.explainer()
-shapValues.explainer_default()
+#shapValues.explainer("knn_regressor")
